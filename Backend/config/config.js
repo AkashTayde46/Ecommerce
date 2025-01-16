853e5d4c564655da22510cf68bb25effc12a3233
Akash Tayde <akashtayde341@gmail.com>
2025-01-16 23:04:09 +0530

    Initial commit

--- Backend/config/config.js
+++
@@ -1,7 +0,0 @@
-const JWT_SECRET = "hkjjanakjsckjsncoiuhfkjxcnoiWUECNH"; // Hardcoded secret
-const SMPT_MAIL = "akashtayde360@gmail.com"
-const SMPT_PASSWORD = "xsfz hwrb dtli pkij"
-const SMPT__SERVICE = "gmail"
-const SMPT_HOST = "smpt.gmail.com"
-const SMPT_PORT = 465
-module.exports = { JWT_SECRET, SMPT_MAIL,SMPT_PASSWORD,SMPT__SERVICE, SMPT_HOST,SMPT_PORT };
